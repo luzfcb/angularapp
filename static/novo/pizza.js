751e68af5d1ce91671e20b3b03957f9c227c76ee
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2014-09-26 19:16:01 -0300

    mais

--- static/novo/pizza.js
+++ static/novo/pizza.js
@@ -1,3 +1,4 @@
+//http://uncorkedstudios.com/blog/multipartformdata-file-upload-with-angularjs
 $('.ui.selection.dropdown').dropdown();
 $('.foto.image')
   .transition('fade up')
@@ -49,3 +50,45 @@ pizza_app.directive('pizzaImg', function() {
         template: '<img class="foto image" src="{{ src }}" width="130">'
     };
 });
+
+pizza_app.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
+
+pizza_app.service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function(file, uploadUrl){
+        var fd = new FormData();
+        fd.append('file', file);
+        $http.post(uploadUrl, fd, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        })
+        .success(function(){
+        })
+        .error(function(){
+        });
+    }
+}]);
+
+pizza_app.controller('formController', ['$scope', 'fileUpload', function($scope, fileUpload){
+    var base_url_api = 'http://127.0.0.1:8000';
+    $scope.uploadFile = function(){
+        var file = $scope.myFile;
+        console.log('file is ' + JSON.stringify(file));
+        var uploadUrl = base_url_api +'/pizzas/';
+        fileUpload.uploadFileToUrl(file, uploadUrl);
+    };
+
+}]);
